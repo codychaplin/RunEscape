950aa0cc7be61cda5cfa7ccb4a48c871ce0f7d7a
Cody Chaplin <chaplinbcody@gmail.com>
2021-08-08 16:06:04 -0400

    Initial Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+
+    // zoom
+    float zoom = 8f;
+    public float zoomSpeed = 4f;
+    public float minZoom = 6f;
+    public float maxZoom = 12f;
+
+    // rotate
+    public float pitch = 2f;
+    public float yawSpeed = 0f;
+
+    float yaw = 100f;
+
+    void Update()
+    {
+        zoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+
+        yaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.position - offset * zoom; // moves camera
+        transform.LookAt(player.position + Vector3.up * pitch); // aims camera at player
+        transform.RotateAround(player.position, Vector3.up, yaw); // rotates camera
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Player : MonoBehaviour
+{
+    public LayerMask excludeMask;
+
+    Camera cam;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // if left click (move)
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                MoveToPoint(new Vector3Int(Mathf.RoundToInt(hit.point.x), Mathf.RoundToInt(hit.point.y), Mathf.RoundToInt(hit.point.z)));
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1)) // if right click (interact)
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                
+            }
+        }
+    }
+
+    void MoveToPoint(Vector3Int point)
+    {
+        agent.SetDestination(point);
+    }
+}
