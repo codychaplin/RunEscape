c50daa6a7763466fad7c62a0e04fb45de85f6c54
Cody Chaplin <chaplinbcody@gmail.com>
2021-08-08 23:56:30 -0400

    Created interactable foundation

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 2f;
+    public Transform interactionTransform;
+
+    Transform player;
+    bool isFocus = false;
+    bool hasInteracted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFocus && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if (distance <= radius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+    }
+
+    public virtual void Interact()
+    {
+        Debug.Log("interacting with " + transform.name);
+    }
+
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDefocused()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class Player : MonoBehaviour
 {
+    public Interactable focus;
     public LayerMask excludeMask;
 
     Camera cam;
     NavMeshAgent agent;
+    Transform target;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +29,7 @@ public class Player : MonoBehaviour
             if (Physics.Raycast(ray, out hit, 100))
             {
                 MoveToPoint(new Vector3Int(Mathf.RoundToInt(hit.point.x), Mathf.RoundToInt(hit.point.y), Mathf.RoundToInt(hit.point.z)));
+                RemoveFocus();
             }
         }
 
@@ -39,13 +40,66 @@ public class Player : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit, 100))
             {
-                
+                // if focus is interactable, set focus
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if (interactable != null)
+                    SetFocus(interactable);
             }
+        }
+
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+            FaceTarget();
         }
     }
 
     void MoveToPoint(Vector3Int point)
     {
         agent.SetDestination(point);
+    }
+
+    void FollowTarget(Interactable newTarget)
+    {
+        agent.stoppingDistance = newTarget.radius;
+        agent.updateRotation = false;
+        target = newTarget.interactionTransform;
+    }
+
+    void UnFollowTarget()
+    {
+        agent.stoppingDistance = 0f;
+        agent.updateRotation = true;
+        target = null;
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    void SetFocus(Interactable newFocus)
+    {
+        if (newFocus != focus)
+        {
+            if (focus != null)
+                focus.OnDefocused();
+
+            focus = newFocus;
+            FollowTarget(newFocus);
+        }
+
+        newFocus.OnFocused(transform);
+    }
+
+    void RemoveFocus()
+    {
+        if (focus != null)
+            focus.OnDefocused();
+
+        focus = null;
+        UnFollowTarget();
     }
 }
