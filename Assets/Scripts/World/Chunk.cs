ab47a450d21dca5f057cd887884eef26ecf284ce
Cody Chaplin <chaplinbcody@gmail.com>
2021-09-03 21:50:08 -0400

    Implemented basic view distance

---
+++ Assets/Scripts/World/Chunk.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Chunk
+{
+    Transform chunk;
+    
+    public int x { get; private set; }
+    public int z { get; private set; }
+
+    bool _isActive;
+
+    public Chunk(Transform _chunk, int _x, int _z)
+    {
+        chunk = _chunk;
+        x = _x;
+        z = _z;
+    }
+
+    public bool isActive
+    {
+        get { return _isActive; }
+        set
+        {
+            _isActive = value;
+            if (chunk != null)
+                chunk.gameObject.SetActive(value);
+        }
+    }
+}
--- Assets/Scripts/World/World.cs
+++ Assets/Scripts/World/World.cs
@@ -9,7 +9,8 @@ public class World : MonoBehaviour
     public Material material;
 
     public static readonly int ChunkWidth = 16;
-    public static readonly int WorldSizeInChunks = 2;
+    public static readonly int WorldSizeInChunks = 6;
+    static readonly int ViewDistance = 1;
 
     // used to get the world size in tiles, given size of world in chunks
     public static int WorldSizeInTiles
@@ -21,19 +22,27 @@ public class World : MonoBehaviour
     public static Tile[,] tileMap = new Tile[WorldSizeInTiles, WorldSizeInTiles];
 
     // array of chunks in game
-    Transform[,] chunks = new Transform[WorldSizeInChunks, WorldSizeInChunks];
+    Chunk[,] chunks = new Chunk[WorldSizeInChunks, WorldSizeInChunks];
+    List<Chunk> activeChunks = new List<Chunk>();
+    Chunk playerChunkPos;
+    Chunk lastPlayerChunkPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        InitWorld();
-        GetData();
+        InitWorld(); // initialize tileMap and chunks
+        GetObstacles(); // get unwalkable positions
+
+        lastPlayerChunkPos = GetChunk(player.position); // set chunk player is on
+        CheckViewDistance();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        playerChunkPos = GetChunk(player.position); // update player chunk position
+        if (playerChunkPos != lastPlayerChunkPos) // if on different chunk
+            CheckViewDistance();
     }
 
     void InitWorld()
@@ -47,15 +56,27 @@ public class World : MonoBehaviour
 
         foreach (Transform child in chunkParent) // foreach child in parent
             if (child.tag == "Chunk") // if tag = Chunk
-                chunkList.Add(child); // add to list
+                if (!child.GetComponent<MeshCollider>()) // add MeshCollider
+                {
+                    MeshCollider meshCollider = child.gameObject.AddComponent<MeshCollider>();
+                    Mesh mesh = child.GetComponent<MeshFilter>().mesh;
+                    meshCollider.sharedMesh = mesh;
+                    child.gameObject.SetActive(false);
+                    chunkList.Add(child); // add to list
+                }
 
-        int index = 0;
-        for (int i = 0; i < chunks.GetLength(0); i++)
-            for (int j = 0; j < chunks.GetLength(1); j++)
-                chunks[i, j] = chunkList[index++]; // transfers list to array
+        if (chunkList.Count == chunks.Length)
+        {
+            int index = 0;
+            for (int i = 0; i < chunks.GetLength(0); i++)
+                for (int j = 0; j < chunks.GetLength(1); j++)
+                    chunks[i, j] = new Chunk(chunkList[index++], i, j); // transfers list to array
+        }
+        else
+            Debug.Log("Chunks list/array not same size");
     }
 
-    void GetData()
+    void GetObstacles()
     {
         StreamReader textIn = new StreamReader(new FileStream(@"Assets\WorldData\Obstacles.txt", FileMode.OpenOrCreate, FileAccess.Read));
 
@@ -92,13 +113,60 @@ public class World : MonoBehaviour
         }
     }
 
+    void CheckViewDistance()
+    {
+        Chunk chunkPos = GetChunk(player.position);
+        if (chunkPos != null)
+        {
+            lastPlayerChunkPos = playerChunkPos;
+
+            List<Chunk> previouslyActiveChunks = new List<Chunk>(activeChunks);
+            activeChunks.Clear();
+
+            for (int i = Mathf.Max(0, chunkPos.x - ViewDistance); i <= Mathf.Min(WorldSizeInChunks - 1, chunkPos.x + ViewDistance); i++)
+                for (int j = Mathf.Max(0, chunkPos.z - ViewDistance); j <= Mathf.Min(WorldSizeInChunks - 1, chunkPos.z + ViewDistance); j++)
+                {
+                    Chunk thisChunk = chunks[i, j];
+                    if (thisChunk != null)
+                    {
+                        thisChunk.isActive = true;
+                        activeChunks.Add(thisChunk);
+                    }
+
+                    for (int k = 0; k < previouslyActiveChunks.Count; k++)
+                        if (previouslyActiveChunks[k] == thisChunk)
+                            previouslyActiveChunks.RemoveAt(k);
+                }
+
+            foreach (Chunk chunk in previouslyActiveChunks)
+            {
+                chunks[chunk.x, chunk.z].isActive = false;
+            }
+        }
+    }
+
     public static Tile GetTile(int x, int z)
     {
         return tileMap[x, z];
     }
 
-    void OnDrawGizmosSelected()
+    bool IsInWorld(Vector3 pos)
     {
+        if (pos.x >= 0 && pos.x < WorldSizeInTiles && pos.z >= 0 && pos.z < WorldSizeInTiles)
+            return true;
+        else
+            return false;
+    }
 
+    Chunk GetChunk(Vector3 pos)
+    {
+        if (IsInWorld(pos))
+        {
+            int x = Mathf.Abs(Mathf.FloorToInt(pos.x / ChunkWidth));
+            int z = Mathf.Abs(Mathf.FloorToInt(pos.z / ChunkWidth));
+            return chunks[x, z];
+        }
+        else
+            return null;
     }
 }
